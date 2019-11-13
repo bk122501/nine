df3df8f7af2133785b0b232388abee74505b5342
bk122501 <bk122501@163.com>
2019-11-13 20:09:37 +0800

    ss

---
+++ upload/file_list.php
@@ -0,0 +1,13 @@
+<?php 
+$arr = $_FILES;
+$tmpName= $arr['data']['tmp_name'];
+$fileName="111.jpg";
+$content = file_get_contents($tmpName);
+file_put_contents($fileName,$content,FILE_APPEND);
+
+$res = array(
+		"error"=>0
+	);
+echo json_encode($res);
+
+ ?>
